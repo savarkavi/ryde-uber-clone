b84d91e67fcf61e6d93873f541e6bc1787dc0fa7
Sushant Sharma <sushant20.sharma00@gmail.com>
2024-09-14 09:41:50 +0530

    first commit

---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,13 @@
+import { Text } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Welcome = () => {
+  return (
+    <SafeAreaView>
+      <Text>Welcome</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Welcome;
