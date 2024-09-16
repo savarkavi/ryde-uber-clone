0812ed0a33c7817edf5c2f52bceaba1b1e81d291
Sushant Sharma <sushant20.sharma00@gmail.com>
2024-09-16 15:25:02 +0530

    updated

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,11 +1,27 @@
-import { Text } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "@/constants";
+import InputField from "@/components/InputField";
 
 const SignUp = () => {
   return (
     <SafeAreaView>
-      <Text>SignUp</Text>
+      <ScrollView className="bg-white h-full">
+        <View className="relative">
+          <Image
+            source={images.signUpCar}
+            resizeMode="contain"
+            className="w-full h-[250px]"
+          />
+          <Text className="text-2xl font-JakartaSemiBold font-semibold absolute bottom-3 left-5">
+            Create Your Account
+          </Text>
+        </View>
+        <View>
+          <InputField label="Name" />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
--- components/InputField.tsx
+++ components/InputField.tsx
@@ -1,13 +1,35 @@
-import { View, Text, KeyboardAvoidingView } from "react-native";
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Image,
+  TextInput,
+} from "react-native";
 import React from "react";
-import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 
-const InputField = ({ label }) => {
+const InputField = ({
+  label,
+  labelStyle,
+  icon,
+  containerStyles,
+  inputStyles,
+}) => {
   return (
     <KeyboardAvoidingView>
       <TouchableWithoutFeedback>
-        <View>
-          <Text>{label}</Text>
+        <View className="flex flex-col space-y-3 p-4">
+          <View className={`${labelStyle}`}>
+            <Text className="font-semibold font-JakartaSemiBold">{label}</Text>
+          </View>
+          <View
+            className={`border border-neutral-200 bg-neutral-100 py-2 rounded-lg ${containerStyles}`}
+          >
+            {icon && (
+              <Image source={icon} className="w-6 h-6" resizeMode="contain" />
+            )}
+            <TextInput className={`${inputStyles}`} />
+          </View>
         </View>
       </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
